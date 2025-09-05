0f3363d13e1f213fb9efceda1a9ad0ddf88d05c8
Michael G. <michaelgiu2012@outlook.com>
2025-09-05 16:00:09 -0400

    Update Vector2.java

--- Vectors/src/main/java/Vector2.java
+++ Vectors/src/main/java/Vector2.java
@@ -7,10 +7,19 @@ public class Vector2 {
     }
 
     public Vector2 add(Vector2 add) {
-        point1 = this.point1.add(add.point1);
-        point2 = this.point2.add(add.point2);
+        Point point1 = this.point1.add(add.point1);
+        Point point2 = this.point2.add(add.point2);
         return new Vector2(point1, point2);
     }
 
-    public Vector2 subtract(Vector2)
+    public Vector2 subtract(Vector2 subtract) {
+        Point point1 = this.point1.add(new Point(-add.point1.x, -add.point1.y));
+        Point point2 = this.point2.add(new Point(-add.point2.x, -add.point2.y));
+        return new Vector2(point1, point2);
+    }
+
+    public double getMagitude() {
+        
+    }
 }
+
